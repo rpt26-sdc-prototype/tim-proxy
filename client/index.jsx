e1e61faab50f7ec4f685d88bb246887399a92b72
Tim Anderson <tdanderson805@gmail.com>
2021-03-20 12:14:53 -0700

    adding bundle routes through proxy

---
+++ client/index.jsx
@@ -0,0 +1,7 @@
+import HeaderApp from './components/headerApp.jsx';
+import FooterApp from './components/footerApp.jsx';
+
+console.log('running the proxy bundle')
+
+window.HeaderApp = HeaderApp;
+window.FooterApp = FooterApp;
--- server/index.js
+++ server/index.js
@@ -23,8 +23,56 @@ app.get('/:id', (req, res) => {
   //through proxy then to server
   //check for errors before returning res.send()
 
+//images routes
+
+app.get('/photoBundle/index_bundle.js', async (req, res) => {
+  var {data} = await axios.get('http://localhost:4012/index_bundle.js')
+  res.send(data)
+})
+
+app.get('/images/:id', async (req, res) => {
+  let {data} = await axios.get(`http://localhost:4012/images/${req.params.id}`);
+  res.send(data);
+});
+
+//product routes
+app.get('/metaBundle/index.js', async (req, res) => {
+  var {data} = await axios.get('http://localhost:4032/index.js')
+  res.send(data)
+})
+
+app.get('/api/product/:id/', async (req, res) => {
+  let {data} = await axios.get(`http://localhost:4032/api/product/${req.params.id}`);
+  res.send(data);
+});
+
+
+//more like this routes
+app.get('/moreLikeThisBundle/bundle.js', async (req, res) => {
+  var {data} = await axios.get('http://localhost:4022/bundle.js')
+  res.send(data)
+})
+
+app.get('/morelikethis/:id', async (req, res) => {
+  await axios.get(`http://localhost:4022/morelikethis/${req.params.id}`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('Error with GET request to server', err);
+      res.status(404).end();
+    });
+});
+
+//review routes
 app.get('/reviews/:id', async (req, res) => {
-  var {data} = await axios.get(`http://localhost:3001/reviews/${req.params.id}`)
+  var {data} = await axios.get(`http://localhost:4052/reviews/${req.params.id}`)
+  res.send(data)
+})
+
+//this allows the proxy to get the bundle.js file from the service
+app.get('/reviewBundle/bundle.js', async (req, res) => {
+  var {data} = await axios.get('http://localhost:4052/bundle.js')
   res.send(data)
 })
 
