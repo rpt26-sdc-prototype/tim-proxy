aad0fa0f76e62a89fae916cf0e18673ed951c43c
James Hrivnak <jameshrivnak4@gmail.com>
2021-06-11 14:39:03 -0600

    getting ecosystem.config to work

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:rpt26-sdc-prototype/tim-proxy.git',
       path: '/home/ubuntu/sdc-proxy',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'npm install && pm2 startOrRestart ./server/ecosystem.config.js'
     }
   }
 }
